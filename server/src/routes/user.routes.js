86504d94e155ac5d187df2fc4a7cd9f293075b0a
KIRAN-BUSARI <kiranbusari2208@gmail.com>
2024-01-30 19:51:42 +0530

    Added the get User logic and route

--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { loginUser, registerUser, logoutUser, changePassword } from "../controllers/userController.js";
+import { loginUser, registerUser, logoutUser, changePassword, getCurrentUser } from "../controllers/userController.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
@@ -8,5 +8,6 @@ router.route("/signup").post(registerUser);
 router.route("/login").post(loginUser);
 router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/changePassword").post(verifyJWT, changePassword)
+router.route("/getUser").get(getCurrentUser)
 
 export default router;
